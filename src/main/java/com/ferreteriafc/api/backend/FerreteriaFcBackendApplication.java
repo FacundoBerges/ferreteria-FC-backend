e5bba4da6dfd1b91b911bfc05ea23490457d9076
Facundo Berges <facundo.h.berges@gmail.com>
2024-12-18 16:57:41 -0300

    Estructura base del proyecto y archivos de configuración

---
+++ src/main/java/com/ferreteriafc/api/backend/FerreteriaFcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ferreteriafc.api.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class FerreteriaFcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerreteriaFcBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferreteriafc/api/backend/FerreteriaFcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteriafc.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaFcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
