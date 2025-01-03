90ffcb297d30507c87fe40ab90cf14a0c185c896
FacundoBerges <facundo.h.berges@gmail.com>
2025-01-03 14:46:55 -0300

    Desarrollo de lógica de servicio de marcas

--- src/main/java/com/ferreteriafc/api/backend/domain/service/BrandServiceImpl.java
+++ src/main/java/com/ferreteriafc/api/backend/domain/service/BrandServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ferreteriafc.api.backend.domain.mapper.IBrandMapper;
+import com.ferreteriafc.api.backend.persistence.entity.Brand;
 import com.ferreteriafc.api.backend.persistence.repository.BrandRepository;
 import com.ferreteriafc.api.backend.web.dto.BrandDTO;
 import com.ferreteriafc.api.backend.web.dto.request.SaveBrandDTO;
@@ -24,26 +25,46 @@ public class BrandServiceImpl implements IBrandService {
 
     @Override
     public List<BrandDTO> findAll() {
-        return List.of();
+        List<Brand> brands = brandRepository.findAll();
+
+        if (brands.isEmpty())
+            throw new RuntimeException("Brand not found");
+
+        return brandMapper.toBrandDTOList(brands);
     }
 
     @Override
     public BrandDTO findById(Long id) {
-        return null;
+        Brand brand = brandRepository.findById(id).orElseThrow( () -> new RuntimeException("Brand does not exist"));
+
+        return brandMapper.toBrandDTO(brand);
     }
 
     @Override
     public BrandDTO save(SaveBrandDTO brandDTO) {
-        return null;
+        String brandName = brandDTO.getBrandName();
+
+        if (brandRepository.existsByName(brandName))
+            throw new RuntimeException("Brand already exists");
+
+        Brand brand = brandMapper.toBrand(brandDTO);
+
+        return brandMapper.toBrandDTO(brandRepository.save(brand));
     }
 
     @Override
     public BrandDTO update(BrandDTO brandDTO) {
-        return null;
+        Brand brand = brandMapper.toBrand(brandDTO);
+
+        if(! brandRepository.existsById(brand.getId()))
+            throw new RuntimeException("Brand not found");
+
+        return brandMapper.toBrandDTO(brandRepository.save(brand));
     }
 
     @Override
     public void delete(Long id) {
-
+        brandRepository.deleteById(id);
     }
+
 }
