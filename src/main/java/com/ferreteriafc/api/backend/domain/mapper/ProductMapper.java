a5346567e42c68f29ef60ffb73481e12a8dab3f8
Facundo Berges <facundo.h.berges@gmail.com>
2025-01-06 12:37:37 -0300

    Creación de ProductMapper

--- src/main/java/com/ferreteriafc/api/backend/domain/mapper/ProductMapper.java
+++ src/main/java/com/ferreteriafc/api/backend/domain/mapper/ProductMapper.java
@@ -1,4 +1,13 @@
 package com.ferreteriafc.api.backend.domain.mapper;
 
+import org.mapstruct.Mapper;
+
+@Mapper(
+    componentModel = "spring",
+    uses = { CategoryMapper.class, BrandMapper.class }
+)
 public interface ProductMapper {
+
+
+
 }
