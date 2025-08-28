eda9afa00ba8e774473927aae865d4b1366711a1
Facundo Berges <facundo.h.berges@gmail.com>
2025-08-28 15:32:31 -0300

    FIX: añadida anotación para repositorio

--- src/main/java/com/ferreteriafc/api/backend/persistence/repository/RoleRepository.java
+++ src/main/java/com/ferreteriafc/api/backend/persistence/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.ferreteriafc.api.backend.persistence.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ferreteriafc.api.backend.persistence.entity.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Optional<Role> findByName(String name);
