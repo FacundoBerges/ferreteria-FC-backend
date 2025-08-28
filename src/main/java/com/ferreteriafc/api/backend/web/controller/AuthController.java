0919a316a8c2df43679201a1366713649e2dc3c7
Facundo Berges <facundo.h.berges@gmail.com>
2025-08-28 15:09:42 -0300

    ADD: añadido controller para login y registro de usuarios.

--- src/main/java/com/ferreteriafc/api/backend/web/controller/AuthController.java
+++ src/main/java/com/ferreteriafc/api/backend/web/controller/AuthController.java
@@ -5,30 +5,51 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.ferreteriafc.api.backend.domain.service.IUserService;
-import com.ferreteriafc.api.backend.web.dto.request.RegisterAndLoginUserDTO;
+import com.ferreteriafc.api.backend.web.dto.request.LoginUserDTO;
+import com.ferreteriafc.api.backend.web.dto.request.RegisterUserDTO;
+import com.ferreteriafc.api.backend.web.dto.response.AuthToken;
+import com.ferreteriafc.api.backend.web.security.jwt.JwtUtils;
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthenticationManager authenticationManager;
     private final IUserService userService;
+    private final JwtUtils jwtUtils;
 
     @Autowired
-    public AuthController(IUserService userService) {
+    public AuthController(AuthenticationManager authenticationManager, IUserService userService, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
         this.userService = userService;
+        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterAndLoginUserDTO user) {
-        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterUserDTO user) {
+        String token = jwtUtils.generateToken(userService.signUp(user));
+
+        return new ResponseEntity<>(new AuthToken(token), HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody @Valid RegisterAndLoginUserDTO user) {
-        return new ResponseEntity<>(userService.login(user), HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody @Valid LoginUserDTO user) {
+        Authentication usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(user.getUsernameOrEmail(), user.getPassword());
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        AuthToken authToken = new AuthToken(jwtUtils.generateToken(userDetails));
+
+        return new ResponseEntity<>(authToken, HttpStatus.OK);
     }
 
 }
