470495553c70cdf1e1c6e94edf171afd3544f9ee
Facundo Berges <facundo.h.berges@gmail.com>
2025-08-28 20:23:56 -0300

    FIX: cambios de métodos en interfaz

--- src/main/java/com/ferreteriafc/api/backend/domain/service/IUserService.java
+++ src/main/java/com/ferreteriafc/api/backend/domain/service/IUserService.java
@@ -1,19 +1,21 @@
 package com.ferreteriafc.api.backend.domain.service;
 
-import com.ferreteriafc.api.backend.web.dto.response.AuthToken;
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.ferreteriafc.api.backend.web.dto.request.RegisterUserDTO;
 import com.ferreteriafc.api.backend.web.dto.UserDTO;
-import com.ferreteriafc.api.backend.web.dto.request.RegisterAndLoginUserDTO;
 
 public interface IUserService extends UserDetailsService {
 
-    UserDTO save(RegisterAndLoginUserDTO userDTO);
-
-    AuthToken login(RegisterAndLoginUserDTO userDTO);
-
+    UserDetails signUp(RegisterUserDTO userDTO);
+    List<UserDTO> findAll();
+    UserDTO findById(Integer id);
     UserDTO findByUsername(String username);
-
     UserDTO findByEmail(String email);
+    UserDTO update(Integer id, UserDTO userDTO);
+    void delete(Integer id);
 
 }
